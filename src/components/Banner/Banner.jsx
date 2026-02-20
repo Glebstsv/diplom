6550ee0d0d3385d03933376e3dfef56f30d92701
Glebstsv <glebsstasevich@gmail.com>
2026-02-20 16:50:51 +0300

    Fix css for wide screen

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,14 +2,18 @@ import "./Banner.css";
 
 const Banner = ({ name, link, children }) => {
   return (
-    <div className={`banner banner--${name}`}>
-      <img src={link} alt="" className="banner__image" />
-      {name === "home" && (
-        <h2 className="banner__title">
-          Вся жизнь - <strong>путешествие!</strong>
-        </h2>
-      )}
-      {children}
+    <div
+      className={`banner banner--${name}`}
+      style={name !== "home" ? { backgroundImage: `url(${link})` } : {}}
+    >
+      <div className="banner__content">
+        {name === "home" && (
+          <h2 className="banner__title">
+            Вся жизнь - <strong>путешествие!</strong>
+          </h2>
+        )}
+        {children}
+      </div>
     </div>
   );
 };
