e70431c879ca186eafd780407bb3f4d579072da0
Glebstsv <glebsstasevich@gmail.com>
2026-02-21 13:06:30 +0300

    fix Banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,10 +2,12 @@ import "./Banner.css";
 
 const Banner = ({ name, link, children }) => {
   return (
-    <div
-      className={`banner banner--${name}`}
-      style={name !== "home" ? { backgroundImage: `url(${link})` } : {}}
-    >
+    <div className={`banner banner--${name}`}>
+      <img src={link} alt="" className="banner__image" />
+      <div className="banner__overlay"></div>
+
+      {name === "home" && <div className="banner__footer-line"></div>}
+
       <div className="banner__content">
         {name === "home" && (
           <h2 className="banner__title">
